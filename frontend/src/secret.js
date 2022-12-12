3dd457c5bb8340117b20c399806a9dac22cd153d
Moises <daw.moisesguerola@gmail.com>
2022-12-12 18:52:01 +0100

    (Working) - Client Reserves

--- frontend/src/secret.js
+++ frontend/src/secret.js
@@ -1,5 +1,5 @@
 export default {
     ADMIN_SERVER: "http://localhost:3001",
-    CLIENT_SERVER: "http://localhost:8080/api",
+    CLIENT_SERVER: "http://localhost:8080/api", // Change to 3000 when Docker
     LOCALHOST: "http://localhost:5173"
 }
