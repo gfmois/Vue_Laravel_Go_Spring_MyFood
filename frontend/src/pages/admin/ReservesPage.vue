89a6bd9cf2d792328ee279da3cf43cfccd911094
JoaquimRS <joaquimdaweb@gmail.com>
2023-01-03 02:35:30 +0100

    Merge Issues

--- frontend/src/pages/admin/ReservesPage.vue
+++ frontend/src/pages/admin/ReservesPage.vue
@@ -26,28 +26,6 @@ export default {
          <v-icon name="bi-qr-code-scan" scale="2" @click="clicked = true" />
         <QrReaderComponentVue @open_close="clicked = $event" v-if="clicked" />
     </div>
-<<<<<<< HEAD
-    <table>
-        <thead>
-            <tr>
-                <th>Cliente</th>
-                <th>Fecha</th>
-                <th>Nº Comensales</th>
-                <th>Servicio</th>
-                <th>Estado</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="i in 5">
-                <td>Hola</td>
-                <td>Hola</td>
-                <td>Hola</td>
-                <td>Hola</td>
-                <td>Hola</td>
-            </tr>
-        </tbody>
-    </table>
-=======
     <div class="qr-reader">
         <v-icon name="bi-qr-code-scan" scale="2" @click="clicked = true" />
     </div>
@@ -55,7 +33,6 @@ export default {
     <div v-if="$route.params.id">
         <ReserveFormComponent />
     </div>
->>>>>>> mois-dev
 </div>
 </template>
 <style scoped>
@@ -84,52 +61,5 @@ export default {
     width: 100%;
     border-radius: 10px;
 }
-<<<<<<< HEAD
-table {
-    width: 100%;
-    position: relative;
-    border-radius: 20px;
-    text-align: center;
-}
-* {
-    border-collapse: collapse;
-    box-sizing: border-box;
-}
-th,td {
-    padding: 15px;
-    font-size: 1.2rem;
-}
-th {
-    top: 0;
-    position: sticky;
-    background-color: #362e4a;
-    padding: 10px;
-    color: white;
-}
-
-tbody {
-    overflow-y: scroll;
-}
-tr:nth-child(2n) {
-    background-color: #f6f7f7;
-}
-@media (min-device-width: 768px){
-    .qr-reader {
-        display: none;
-    }
-}
-@media (max-width: 500px) {
-    .main {
-        border-radius: 0px;
-    }
-    .qr-reader {
-        top: 10px;
-    }
-    th:nth-child(3), td:nth-child(3){
-        display: none;
-    }
-}
-=======
->>>>>>> mois-dev
 
 </style>
